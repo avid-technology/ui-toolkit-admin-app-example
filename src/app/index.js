05463e065306083ba4094db14827ff78ac61172d
Tomek Moryl <ramalash@gmail.com>
2018-07-13 13:36:46 +0200

    Working fetch

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,27 @@
 /**
  * Copyright 2017 by Avid Technology, Inc.
  */
-import { getLocalization } from 'cloudux-l10n';
+import {getLocalization} from 'cloudux-l10n';
 import l10nData from '../l10n/lang.en.json';
 
 export default class ApplicationContainer {
+
+
     constructor() {
-        this.div = document.createElement('div');
-        this.div.innerHTML = getLocalization(l10nData)('example-plugin-message');
+        fetch('https://127.0.0.1:8080/apis/avid.acs.attributes;realm=global;version=3/attributes?name=tomek', {
+            credentials: "same-origin"
+        }).then((res) => {
+            this.div = document.createElement('div');
+            this.div.innerHTML = 'tomek';
+        })
     }
 
     returnElement() {
         return this.div;
     }
+
+}
+
+async function getUser() {
+    return await fetch('https://127.0.0.1:8080/apis/avid.acs.attributes;realm=global;version=3/attributes?name=tomek')
 }
